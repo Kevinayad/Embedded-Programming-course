709def85644546240b7c09a032cc24c3ec6ab3fb
Kevin Ayad <kevinayad@Kevins-MacBook-Pro.local>
2022-02-02 19:41:16 +0100

    done e1

--- assignment2/ass2e1.c
+++ assignment2/ass2e1.c
@@ -1,20 +1,20 @@
 #include<stdio.h>
 #include<string.h>
-    enum DIRECTION {N,O,S,W}; 
+    enum DIRECTION {N,O,S,W}; //Direction list
  
-typedef struct { 
+typedef struct { //define ROBOT
         int xpos; 
         int ypos; 
         enum DIRECTION dir; 
 } ROBOT; 
-void move(int *x){
+void move(int *x){//move x axis one step forward
 if(*x>=0&&*x<99){
     *x=*x+1;
 }else{
     printf("cannot add anymore steps\n");
 }
 }
-void turn(int *c){
+void turn(int *c){//turn the direction
     if(*c>=0||*c<4){
         *c=*c+1;
     }else{
@@ -22,12 +22,26 @@ void turn(int *c){
     }
     
 }
+//used to get value from enum
+const char* getDirName(enum DIRECTION dir) //refrence:https://stackoverflow.com/questions/3168306/print-text-instead-of-value-from-c-enum
+{
+   switch (dir) 
+   {
+      case N: return "N";
+      case O: return "O";
+      case S: return "S";
+      case W: return "W";
+      /* etc... */
+   }
+}
+
+
 int main(){
-   ROBOT R1;
-    char choice;
-    R1.dir=0;
-    int c=0;
-    do{
+   ROBOT R1;//robot instance
+    char choice;//char to choose from
+    enum DIRECTION dir=N;//default direction and direction variable
+    int c=0;//counter
+    do{//keep asking for x and y axis if not between 0 and 99
     printf("please input the starting position x coordinate between 0 and 99: \n");
     scanf("%d",&R1.xpos);
     }while(R1.xpos<0||R1.xpos>99);
@@ -35,22 +49,17 @@ int main(){
     printf("please input the starting position y coordinate between 0 and 99: \n");
     scanf("%d",&R1.ypos);
     }while(R1.ypos<0||R1.ypos>99);
-    //printf("%d %d",R1.xpos,R1.ypos);
-    while(choice!='e'){
-         printf("enter m to move one step forward, enter t to to turn directions, enter e to finalize\n");
-         scanf(" %c",&choice);
-         //getchar(choice);
-         printf("%c",choice);
-         if(choice=='m'){
+    while(choice!='e'){//keep repeating if user doesnt want to end
+         printf("enter m to move one step forward, enter t to to turn directions, enter e to finalize\n");//instructions
+         scanf(" %c",&choice);//get choice from user
+         if(choice=='m'){//if m move 1 step
              move(&R1.xpos);
              
-          }else if(choice=='t'){
-              turn(&c);
-              printf("%d\n",c);
-              for(R1.dir= N; R1.dir<=W;R1.dir++){
-                  printf("%d\n",R1.dir);
-                   if(c==R1.dir){
-                       R1.dir=c;
+          }else if(choice=='t'){//if t turn direction
+              turn(&c);//save in c
+              for(R1.dir= N; R1.dir<=W;R1.dir++){//loop through enum
+                   if(R1.dir==c){//set value to variable if match
+                       dir=R1.dir;
                    }
               }
           }
@@ -58,6 +67,7 @@ int main(){
     
     
     }
-    printf("%d %d %d",R1.xpos,R1.ypos,R1.dir);
+    printf("\n");
+    printf("X coordinate: %d Y coordinate: %d Direction: %s\n",R1.xpos,R1.ypos,getDirName(dir));//result
     return 0;
 }
